d3b87759702ea0dff41740593c96bacf3432fae9
Kristoffer Rasmussen <kra@projectum.com>
2022-11-19 11:14:34 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+Stolen from here:
+https://codesandbox.io/s/jxx2n4pp3?file=/src/index.tsx:657-673
+
+https://github.com/christinejordan/ts-sandbox/tree/exercise2/
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// To keep the console relatively clean, you may
+// comment previous exercises as you progress.
+
+//Go go each exercise file in the sidebar and complete it before running it
+
+// ======== Exercise 1: Basic types ========
+import exercise1 from "./1-basic/exercise1";
+exercise1();
+
+// ======== Exercise 2: Interfaces ========
+// import exercise2 from "./2-interfaces/exercise2";
+// exercise2();
+
+// ======== Exercise 3: Functions ========
+// import exercise3 from "./3-functions/exercise3";
+// exercise3();
+
+// ======== Exercise 4: Types ========
+// import exercise4 from "./4-types/exercise4";
+// exercise4();
